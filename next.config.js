6370b0aaca3bbfbfcb1f537ac97cbc96a4a567e2
Alex Sukhotckii <alex.sukhotckii@gmail.com>
2023-03-12 08:17:45 +0300

    update

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: '**.unsplash.com',
-      },
+      }, 
       {
         protocol: 'https',
         hostname: '**.amazonaws.com',
